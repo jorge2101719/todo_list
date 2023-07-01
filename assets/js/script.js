7ad1bd8d2b763a3a29d315b335bc9e6f65169f23
jorge2101719 <rusosky01@gmail.com>
2023-07-01 00:43:13 -0400

    se implementa lógica para agregar tareas con un id y asignación del valor de ellas a Total

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,31 +2,40 @@ let tareaInput = document.querySelector('#tarea_input');
 let agregar = document.querySelector('#agregar');
 let total = document.querySelector('#total');
 let realizadas = document.querySelector('#realizadas');
-let html = '';
+let cuerpoTabla = document.querySelector('#cuerpo_tabla');
+let contarTareas = 0;
+let contarRealizadas = 0;
 
 const tareas = [];
-
-
-
-
 
 agregar.addEventListener('click', function () {
     console.log('click en agregar');
     // console.log(campoIinput.value)
-    if (tareaInput.value === '') {
+    while (tareaInput.value === '') {
         alert('Está tratando de ingresar una tarea vacía, por favor corriga ese detalle')
-    } else {
-    
-        let nueva_tarea = tareaInput.value;
-        tareas.push({
-            id: Date.now(),
-            nombre: nueva_tarea
-        })
-        console.log('la nueva tarea es ', nueva_tarea);
-
-        campoInput = '';
-
     }
-    total.value++
+
+    let nueva_tarea = tareaInput.value;
+    tareas.push({
+        id: Date.now(),
+        nombre: nueva_tarea
+    });
+    tareaInput.value = '';
+
+    let html = ''
+
+    for (let tarea of tareas) {
+        html += `
+            <tr>
+                <td>${tarea.id}</td>
+                <td>${tarea.nombre}</td>
+            </tr>
+        `;
+    }
+
+    cuerpoTabla.innerHTML = html;
+
+    contarTareas = tareas.length;
+    total.innerHTML = contarTareas;
     total.innerHTML
 })
