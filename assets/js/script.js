ca7542b74a57e914647f47066a6421b86b5cc565
jorge2101719 <rusosky01@gmail.com>
2023-07-01 23:53:56 -0400

    se incluye buena parte de la lógica para chequear tareas realizadas: uso de métodos find, findIndex

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,59 @@ let realizadas = document.querySelector('#realizadas');
 let cuerpoTabla = document.querySelector('#cuerpo_tabla');
 let contarTareas = 0;
 let contarRealizadas = 0;
+let tablaTareas = document.querySelector('#tabla_tareas')
+let estadoTarea = false
 
-const tareas = [];
+const tareas = [
+    {
+        id: 16,
+        nombre: 'Ir al mercado',
+        estado: estadoTarea,
+    },
+    {
+        id: 60,
+        nombre: 'Estudiar',
+        estado: estadoTarea,
+    },
+    {
+        id: 24,
+        nombre: 'Pasear al perro',
+        estado: estadoTarea,
+    }
+];
+
+renderTareas();
+totalTareas();
+
 
 function renderTareas() {
     let html = '';
     for (let tarea of tareas) {
         html += `
-            <tr>
+            <tr id=${tarea.id}>
                 <td>${tarea.id}</td>
                 <td>${tarea.nombre}</td>
-                <td><input type='checkbox' id='checkInput' onclick='checkClick()'></td>
-                <td><button id='btnBorrar' onclick='borrar(${tarea.id})'>Borrar Tarea</button></td>
+                <td><input onclick='checkClick(${tarea.id})' type='checkbox' id='${tarea.id}' name='tyc'></td>
+                <td><button onclick='borrar(${tarea.id})' id='btnBorrar'>Borrar Tarea</button></td>
             </tr>
         `;
     }
     cuerpoTabla.innerHTML = html;   
+}
+
+function checkClick (id) {
+    let indexTarea = tareas.findIndex((tarea) => tarea.id == id);
+    let tareaCliqueada = tareas.find((tarea) => tarea.id == id);
+
+    tareas[indexTarea] = {
+        id: tareaCliqueada.id,
+        nombre: tareaCliqueada.nombre,
+        estado: tareaCliqueada.estado == true ? false : true,
+    }
+
+    let tareasRealizadas = tareas.filter((tarea) => tarea.estado === true);
+    contarRealizadas = tareasRealizadas.length;
+    realizadas.innerHTML = contarRealizadas;
 }
 
 function totalTareas () {
@@ -28,49 +65,30 @@ function totalTareas () {
     total.innerHTML = contarTareas;
 }
 
-function checkClick () {
-    let checkBox = document.getElementById("checkInput");
-    // let text = document.getElementById("text");
-    // let contarRealizadas = 0;
-    if (checkBox.checked == true) {
-        contarRealizadas = contarRealizadas + 1;
-        console.log(contarRealizadas);
-        realizadas.innerHTML = contarRealizadas;
-        // text.style.display = "block";
-    } else {
-        // text.style.display = "none";
-        contarRealizadas = contarRealizadas - 1;
-        console.log(contarRealizadas);
-        realizadas.innerHTML = contarRealizadas;
-    }
-}
-
 agregar.addEventListener('click', function () {
-    // console.log('click en agregar');
-    // console.log(campoIinput.value)
     if (tareaInput.value === '') {
         alert('Está tratando de ingresar una tarea vacía, por favor corriga ese detalle')
     } else {
-
         let nueva_tarea = tareaInput.value;
+        estadoTarea = tareas.estado;
         tareas.push({
             id: Date.now(),
-            nombre: nueva_tarea
+            nombre: nueva_tarea,
+            estado: estadoTarea,
         });
+
         tareaInput.value = '';
 
         renderTareas();
         totalTareas();
-    
     }
 })
 
 function borrar(id) {
     const index = tareas.findIndex((ele) => ele.id == id);
     tareas.splice(index, 1);
-    // contarRealizadas = contarRealizadas - 1;
-    // realizadas.innerHTML = contarRealizadas
 
     renderTareas();
     totalTareas();
 }
+
